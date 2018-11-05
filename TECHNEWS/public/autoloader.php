5979dfaaabc7274688f9c10bf5dc68b40f671c15
GauthierBosson <32874699+GauthierBosson@users.noreply.github.com>
2018-11-05 15:48:19 +0100

    Update autoloader.php

--- TECHNEWS/public/autoloader.php
+++ TECHNEWS/public/autoloader.php
@@ -16,7 +16,7 @@ class Autoloader {
 
 /**
  * Fonction static : Appelle la fonction sans instancier la classe, pas besoin de mettre new
-   La fonction static appartient à la classe et non à l'objet
-   Les fonctions static ne peuvent appeler que des fonctions static
+ * La fonction static appartient à la classe et non à l'objet
+ * Les fonctions static ne peuvent appeler que des fonctions static
  * Une fonction non static peut appeler une fonction static en utilisant le mot self
  */
